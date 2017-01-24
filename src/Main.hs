aca823c1e783f71361f191813a63d1851fa2a72b
Zhihong Cheng <chengzh2008@gmail.com>
2017-01-23 21:34:57 -0800

    refactor 2 streaming

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,13 @@
 module Main where
 
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.Vector as V
+import qualified Data.Foldable as F
 
-import Data.Csv
+import Data.Csv.Streaming
 
 type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
 
-baseballStats :: BL.ByteString -> Either String (V.Vector BaseballStats)
+baseballStats :: BL.ByteString -> Records BaseballStats
 baseballStats = decode NoHeader
 
 fourth (_, _, _, d) = d
@@ -15,6 +15,6 @@ fourth (_, _, _, d) = d
 main :: IO ()
 main = do
   csvData <- BL.readFile "batting.csv"
-  let summed = fmap (V.foldr summer 0) (baseballStats csvData)
+  let summed = F.foldr summer 0 (baseballStats csvData)
   putStrLn $ "Total atBats was: " ++ (show summed)
     where summer r n = n + fourth r
